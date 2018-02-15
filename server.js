54e227f1a1222ccf10f30cf9a6e48a603a71dae6
rayan fadhlaoui <rayanfadhlaoui@yahoo.fr>
2018-02-15 19:01:55 +0100

    server.js

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
+
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+
+
+app.use(forceSSL());
 app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
