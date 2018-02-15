e228c9b03545086d7ba01f42e554a1b00d8f5b13
rayan fadhlaoui <rayanfadhlaoui@yahoo.fr>
2018-02-15 18:54:08 +0100

    add redirect to index.html

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
-
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 app.listen(process.env.PORT || 8080);
