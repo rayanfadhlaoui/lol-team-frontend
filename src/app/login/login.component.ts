16b1ebae9c7b5025e2a22d6b7393cec079cb3c4e
Benjamin Christau <benjamin.christau@oyez.fr>
2018-03-05 18:27:00 +0100

    [Prettier] Prettify some code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,41 +1,39 @@
-import { AuthService } from './auth.service';
-import { Component } from '@angular/core';
-import { Router,
-         NavigationExtras } from '@angular/router';
+import { AuthService } from "./auth.service";
+import { Component } from "@angular/core";
+import { Router, NavigationExtras } from "@angular/router";
+
+import { errors } from "./../utils/errors";
 
 @Component({
-   templateUrl: './login.html',
-    styleUrls: ['./login.css']
+  templateUrl: "./login.html",
+  styleUrls: ["./login.css"]
 })
 export class LoginComponent {
-  netImage: any = 'assets/images/Arcade-Riven.png';
+  netImage: any = "assets/images/Arcade-Riven.png";
   messageError: string;
-  user = {'username' : '', 'password': '' };
+  user = { username: "", password: "" };
 
-  constructor(public authService: AuthService, public router: Router) {
-  }
+  constructor(public authService: AuthService, public router: Router) {}
 
   login() {
-    const sub = this.authService.login(this.user).subscribe(
-      res => {
-        const userReturn = res.json();
-        if (userReturn.id !== -1) {
-          this.authService.loggedIn(userReturn.id);
-          const redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/home';
+    const sub = this.authService.login(this.user).subscribe(res => {
+      const userReturn = res.json();
+      if (userReturn.id !== -1) {
+        this.authService.loggedIn(userReturn.id);
+        const redirect = this.authService.redirectUrl
+          ? this.authService.redirectUrl
+          : "/home";
 
-          const navigationExtras: NavigationExtras = {
-              queryParamsHandling: 'preserve',
-              preserveFragment: true
-          };
-          // Redirect the user
-          this.router.navigate([redirect], navigationExtras);
-        } else {
-          this.messageError = 'Username or password is incorrect';
-        }
-      },
-      error => {
-        console.log(error);
-      });
+        const navigationExtras: NavigationExtras = {
+          queryParamsHandling: "preserve",
+          preserveFragment: true
+        };
+        // Redirect the user
+        this.router.navigate([redirect], navigationExtras);
+      } else {
+        this.messageError = "Username or password is incorrect";
+      }
+    });
   }
 
   logout() {
